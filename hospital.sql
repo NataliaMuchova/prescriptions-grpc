ebcd8bb41955dff4de0f8ce6dfb9632f7683db96
NataliaMuchova <nataliiamuchova@gmail.com>
2025-11-30 20:47:32 +0100

    user table updated with patient information and cpr as the only primary key. there is no patient and doctor tables anymore. all tables are changed accordingly.

--- hospital.sql
+++ hospital.sql
@@ -4,31 +4,18 @@ DROP SCHEMA IF EXISTS "hospital" CASCADE;
 CREATE SCHEMA "hospital";
 
 CREATE TABLE "user"(
+    PRIMARY KEY (cpr),
+    role varchar(50) CHECK (role IN ('patient', 'doctor', 'pharmacist')),
     name varchar(100),
     surname varchar(100),
     cpr bigint,
     password varchar(50),
     phone varchar(20),
-    role varchar(50), -- how should we handle a person registered as both doctor/pharmacist and patient? how should our log in look like in front end in relation to this issue?
+    birthday DATE, -- birthday and gender moved to user from patient table that no longer exist
+    gender char(1) CHECK (gender IN ('M', 'F'))
+     -- how should we handle a person registered as both doctor/pharmacist and patient? how should our log in look like in front end in relation to this issue?
     -- if u just log in to the system with cpr, and it logs you into your role that will not work with our architecture?
-    PRIMARY KEY (role, cpr)
-);
-
-CREATE TABLE doctor(
-    id SERIAL PRIMARY KEY,
-    cpr bigint,
-    role varchar,
-    FOREIGN KEY (role, cpr) REFERENCES "user"(role, cpr)
-);
-
-CREATE TABLE patient(
-    id SERIAL PRIMARY KEY,
-    cpr bigint,
-    gender char(1),
-    birthday DATE,
-    role varchar,
-    FOREIGN KEY (role, cpr) REFERENCES "user"(role, cpr)
-);
+    );
 
 CREATE TABLE drug(
     id SERIAL PRIMARY KEY,
@@ -37,14 +24,12 @@ CREATE TABLE drug(
     amount int
 );
 
-
 CREATE TABLE prescription(
     id SERIAL PRIMARY KEY,
     expiration_date DATE,
     issue_date DATE,
-    patient_id bigint REFERENCES patient(id),
-    doctor_id bigint REFERENCES doctor(id)
-
+    patient_cpr bigint REFERENCES "user"(cpr),
+    doctor_cpr bigint REFERENCES "user"(cpr)
 );
 
 CREATE TABLE prescription_drug(
--- hospital_data.sql
+++ hospital_data.sql
@@ -3,21 +3,16 @@ SET search_path = "hospital";
 TRUNCATE "user" CASCADE;
 TRUNCATE drug CASCADE;
 
-INSERT INTO "user" VALUES ('Jena', 'Magenta', 1233445566, '123', '23346789', 'patient');
-INSERT INTO patient VALUES (111, 1233445566, 'F', '2000-09-19', 'patient');
-INSERT INTO "user" VALUES ('John', 'Pork', 1122445533, '456', '11223355', 'patient');
-INSERT INTO patient VALUES (122,1122445533, 'M', '1978-01-16', 'patient');
-
-INSERT INTO "user" VALUES ('Joseph', 'PRO', 1344557788, '789', '9988776655', 'doctor');
-INSERT INTO doctor VALUES (123, 1344557788, 'doctor');
-INSERT INTO "user" VALUES ('Jena', 'Magenta', 1233445566, 'test', '23346789', 'doctor');
-INSERT INTO doctor VALUES (111,1233445566, 'doctor');
+INSERT INTO "user" VALUES('patient', 'Millie','Magenta', 1234567890, '123', 58694725, '2000-09-19', 'M');
+INSERT INTO "user" VALUES('patient', 'Jane','Doe', 1122334455, '112', 23548625, '1999-11-05', 'F');
+INSERT INTO "user" VALUES('doctor', 'John','Travolta', 9988776655, '998', 55224488, '1985-04-30', 'M');
+INSERT INTO "user" VALUES('doctor', 'Mike','Wazowski', 9876543210, '987', 42548745, '1995-06-15', 'M');
 
 INSERT INTO drug VALUES (1,'Xanax', 'It is supposed to help', 21);
 INSERT INTO drug VALUES (2,'Aulin', 'For severe pain', 42);
 
-INSERT INTO prescription VALUES (1,'2028-11-18', now(), 111, 111);
-INSERT INTO prescription VALUES (3,'2029-02-03',now(), 122, 123);
+INSERT INTO prescription VALUES (1,'2028-11-18', now(), 1234567890, 9988776655);
+INSERT INTO prescription VALUES (3,'2029-02-03',now(), 1122334455, 9876543210);
 
 INSERT INTO prescription_drug VALUES (3, 1, 'It is supposed to help', 23, 1);
 INSERT INTO prescription_drug VALUES (5,2, 'For severe pain', 9,3);
