c52b5b39b998bc07a46113f0a12be56156f839c9
pimbp <pimbp@outlook.com>
2025-12-09 16:33:22 +0100

    merge fix 2

---
+++ src/main/java/via/pro3/prescriptionsgrpc/entities/Prescription.java
@@ -0,0 +1,82 @@
+package via.pro3.prescriptionsgrpc.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "prescription", schema = "hospital")
+public class Prescription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "expiration_date")
+    private LocalDate expirationDate;
+
+    @Column(name = "issue_date")
+    private LocalDate issueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_cpr")
+    private User doctor;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_cpr")
+    private User patient;
+
+    @OneToMany(mappedBy = "prescription")
+    private Set<PrescriptionDrug> prescriptionDrugs;
+
+    public User getPatient() {
+        return patient;
+    }
+
+    public void setPatient(User patient) {
+        this.patient = patient;
+    }
+
+    public User getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(User doctor) {
+        this.doctor = doctor;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public Set<PrescriptionDrug> getPrescriptionDrugs()
+    {
+        return prescriptionDrugs;
+    }
+
+    public void setPrescriptionDrugs(Set<PrescriptionDrug> prescriptionDrugs)
+    {
+        this.prescriptionDrugs = prescriptionDrugs;
+    }
+}
---
+++ src/main/java/via/pro3/prescriptionsgrpc/entities/PrescriptionDrug.java
@@ -0,0 +1,75 @@
+package via.pro3.prescriptionsgrpc.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "prescription_drug", schema = "hospital")
+public class PrescriptionDrug {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "drug_id")
+    private Drug drug;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "prescription_id")
+    private Prescription prescription;
+
+    @Column(name = "note", length = 500)
+    private String note;
+
+    @Column(name = "availability_count")
+    private Integer availabilityCount;
+
+    public PrescriptionDrug(Drug drug, Prescription prescription, String note, int availabilityCount)
+    {
+        this.drug = drug;
+        this.prescription = prescription;
+        this.note = note;
+        this.availabilityCount = availabilityCount;
+    }
+
+    public PrescriptionDrug(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Drug getDrug() {
+        return drug;
+    }
+
+    public void setDrug(Drug drug) {
+        this.drug = drug;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Integer getAvailabilityCount() {
+        return availabilityCount;
+    }
+
+    public void setAvailabilityCount(Integer availabilityCount) {
+        this.availabilityCount = availabilityCount;
+    }
+
+    public void setPrescription(Prescription p)
+    {
+        this.prescription = p;
+    }
+}
